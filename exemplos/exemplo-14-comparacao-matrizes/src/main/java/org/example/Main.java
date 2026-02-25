e33925f1f06fb3ede196245e53cae2cdcab7f3e4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-25 11:47:38 -0300

    Exemplo 14 - comparação do conteúdo de matrizes

---
+++ exemplos/exemplo-14-comparacao-matrizes/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] mat2 = {{1, 2, 3}, {4, 5, 6}};
+
+        // Comparar o conteúdo de duas matrizes
+        boolean isEqual = Arrays.deepEquals(mat1, mat2);
+
+
+        System.out.println("As matrizes são iguais? " + isEqual);
+    }
+}
---
+++ exercicios/exercicio-21/src/main/java/org/example/Elemento.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Elemento {
+    private int num;
+
+    public Elemento(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
