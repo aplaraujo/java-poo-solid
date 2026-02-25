45589194558401a746e2fb60aa5a391ba0fdbadc
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-25 15:48:24 -0300

    Exemplo 17 - Matriz oposta

---
+++ exemplos/exemplo-17-matriz-oposta/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, j;
+        int[][] mat = {{1, 2, 3}, {4, 5, 6}};
+
+        System.out.println("==========================");
+        System.out.println("Matriz: ");
+        for(i = 0; i < mat.length; i++) {
+            for(j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Matriz oposta
+        int linhas = mat.length;
+        int colunas = mat[0].length;
+        int[][] oposta = new int[linhas][colunas];
+
+        for(i = 0; i < linhas; i++) {
+            for(j = 0; j < colunas; j++) {
+                oposta[i][j] = -mat[i][j];
+            }
+        }
+
+        System.out.println("==========================");
+        System.out.println("Matriz oposta: ");
+
+        for(i = 0; i < oposta.length; i++) {
+            for(j = 0; j < oposta[0].length; j++) {
+                System.out.print(oposta[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
