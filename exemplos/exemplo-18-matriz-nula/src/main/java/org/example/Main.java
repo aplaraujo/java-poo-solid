37079920088962f2dbfdfa67ff13a81152f9f8da
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-25 16:11:14 -0300

    Exemplo 18 - Matriz nula

--- exemplos/exemplo-18-matriz-nula/src/main/java/org/example/Main.java
+++ exemplos/exemplo-18-matriz-nula/src/main/java/org/example/Main.java
@@ -2,16 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int i, j;
-
-        // Matriz nula
-        int[][] nula = new int[4][4];
-
         System.out.println("=======================");
         System.out.println("Matriz nula: ");
-        for(i = 0; i < nula.length; i++) {
-            for(j = 0; j < nula[0].length; j++) {
-                System.out.print(nula[i][j] + " ");
+        matrizNula(4, 4);
+    }
+
+    public static void matrizNula(int linha, int coluna) {
+        int[][] mat = new int[linha][coluna];
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
