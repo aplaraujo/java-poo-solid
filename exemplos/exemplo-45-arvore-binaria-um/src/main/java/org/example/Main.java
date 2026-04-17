9303dd050a8fc4ce8facc421df605dc06845381b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-04-16 22:05:44 -0300

    Exemplo 45 - Árvore binária sem métodos nativos

--- exemplos/exemplo-45-arvore-binaria-um/src/main/java/org/example/Main.java
+++ exemplos/exemplo-45-arvore-binaria-um/src/main/java/org/example/Main.java
@@ -2,13 +2,69 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Arvore arvore = new Arvore(new Folha(30));
-        arvore.inserir(new Folha(15));
-        arvore.inserir(new Folha(45));
-        arvore.inserir(new Folha(7));
-        arvore.inserir(new Folha(19));
-        arvore.inserir(new Folha(40));
-        arvore.inserir(new Folha(50));
+        Arvore<Integer> arvore = new Arvore<Integer>();
+        arvore.adicionar(10);
+        arvore.adicionar(8);
+        arvore.adicionar(5);
+        arvore.adicionar(9);
+        arvore.adicionar(7);
+        arvore.adicionar(18);
+        arvore.adicionar(13);
+        arvore.adicionar(20);
 
+        System.out.println("==================");
+        System.out.println("==== Em ordem: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(20);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(5);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(8);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(9);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(13);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(7);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+        arvore.remover(18);
+        arvore.remover(10);
+
+        System.out.println("===========================");
+        System.out.println("==== Árvore atualizada: ===");
+        arvore.emOrdem(arvore.getRaiz());
+
+//        System.out.println("===================");
+//        System.out.println("==== Pré-ordem: ===");
+//        arvore.preOrdem(arvore.getRaiz());
+//
+//        System.out.println("===================");
+//        System.out.println("==== Pós-ordem: ===");
+//        arvore.posOrdem(arvore.getRaiz());
     }
 }
