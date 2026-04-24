e49b73cc97f738945218008a11ff6bbb96e96369
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-04-24 18:49:10 -0300

    Exemplo 53 - Palíndromo com dois ponteiros

---
+++ exemplos/exemplo-53-two-pointer-um/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Main {
+    // Verificar se a palavra é um palíndromo
+    public static boolean isPalindrome(String s) {
+        if (s == null) return false;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String someString = "race a car";
+        boolean result = isPalindrome(someString);
+
+        System.out.println(result);
+
+    }
+}
